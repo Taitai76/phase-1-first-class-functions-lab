18543cb0e0ae1f00171b6e76f992763e36e400fa
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-08 23:19:08 -0400

    commit tree 45fb2c83abac04a2f50ec059092af9dd5b03e282

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (x)=>{
+    return x.slice(0,2);
+}
+const returnLastTwoDrivers = (x)=>{
+    return x.slice(-2);
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    function multiplier(y){
+        return y*x;
+    }
+    return multiplier;
+}
+const fareDoubler= createFareMultiplier(2);
+const fareTripler= createFareMultiplier(3);
+
+function selectDifferentDrivers(x, y){
+    let a = []
+    if (x[0]===y(x)[0]){
+        a=[x[0],x[1]];
+        return a;
+    } else {
+        a=[x[2],x[3]];
+        return a;
+    }
+}
